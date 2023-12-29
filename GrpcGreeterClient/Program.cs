1fc2015e9a70538bd704520b9def2250aede6222
Deep Patel <pateldeep691996@gmail.com>
2023-12-29 21:46:26 +0530

    no message

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Net.Client;
+using GrpcService1;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder().
+    SetBasePath(Directory.
+    GetCurrentDirectory()).
+    AddJsonFile("appsettings.json").Build();
+var httpHandler = new HttpClientHandler();
+// Return `true` to allow certificates that are untrusted/invalid  
+httpHandler.ServerCertificateCustomValidationCallback =
+    HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+string serviceUrl = config.GetSection("ServiceUrl").Value ?? "";
+var channel = GrpcChannel.ForAddress(serviceUrl , new GrpcChannelOptions() { HttpHandler = httpHandler});
+
+
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "Patel Deep" });
+Console.WriteLine(reply.Message);
+
+var submitClient = new Submit.SubmitClient(channel);
+var submitReply = await submitClient.RunSubmitAsync(new SubmitRequest() {Message = "This is from client with submit request" });
+Console.WriteLine(submitReply.Message);
+Console.ReadKey();  
---
+++ GrpcService1/Services/SubmitService.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Core;
+
+namespace GrpcService1.Services
+{
+    public class SubmitService : Submit.SubmitBase
+    {
+        public override Task<SubmitResponse> RunSubmit(SubmitRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new SubmitResponse
+            {
+                Message = "This is from all " + request.Message
+            });
+        }
+    }
+}
